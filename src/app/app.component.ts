f9eafaf2e0e4cd6be16b180dee25b54ecc9124de
ADyBaH4uK <104634212+ADyBaH@users.noreply.github.com>
2023-08-02 19:27:31 +0300

    feat(config): eslint setup (EC-14)
    * feat(config): add dependences for eslint(EC-14)
    
    * feat(config): add eslint configs(EC-14)
    
    * refactor(app): apply eslint(EC-14)
    
    * feat(config): add eslint configs(EC-14)
    
    * refactor(app): apply eslint(EC-14)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'ec-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'e-commerce';

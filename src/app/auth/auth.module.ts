7b25a87a9177dead89e6ef3bfc90760f6777d00a
ADyBaH4uK <104634212+ADyBaH@users.noreply.github.com>
2023-08-05 23:40:00 +0300

    refactor(folders): add better folder structure (EC-4)
    * refactor(folders): add better folder structure (EC-4)

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,8 +1,4 @@
-import { CommonModule } from '@angular/common'
 import { NgModule } from '@angular/core'
 
-@NgModule({
-  declarations: [],
-  imports: [CommonModule],
-})
+@NgModule({})
 export class AuthModule {}

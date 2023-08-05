d2a2cf95d287c1da3fe2c08accbafd75ad19c01b
ADyBaH <Brandon4@mail.ru>
2023-08-05 23:04:58 +0300

    refactor(folders): add better folder structure(EC-4)

--- src/app/auth/auth.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core'
-
-@Component({
-  selector: 'ec-auth',
-  templateUrl: './auth.component.html',
-  styleUrls: ['./auth.component.scss'],
-})
-export class AuthComponent {}

26bd22336a5c6a04d998f7e5d1849882e4ff724a
Egor <egormartynov12@gmail.com>
2023-08-07 23:51:36 +0300

    refactor(app): remove useless imports (NO-JIRA)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,11 +9,9 @@ import { NgDompurifySanitizer } from '@tinkoff/ng-dompurify'
 
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
-import { AuthComponent } from './auth/auth.component'
-import { CartComponent } from './cart/cart.component'
 
 @NgModule({
-  declarations: [AppComponent, AuthComponent, CartComponent],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,

86eea7340525346b58d41d2c50951d4d3ccfc85a
Egor <egormartynov12@gmail.com>
2023-08-13 18:52:59 +0300

    feat(header): add basic header layout (EC-44)

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -1,11 +1,25 @@
+import { CommonModule } from '@angular/common'
 import { Component } from '@angular/core'
 import { RouterLink } from '@angular/router'
+import { TUI_BUTTON_OPTIONS, TuiButtonModule, TuiSvgModule } from '@taiga-ui/core'
+import { of, shareReplay } from 'rxjs'
 
 @Component({
   selector: 'ec-header',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, TuiButtonModule, TuiSvgModule, CommonModule],
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
+  providers: [
+    {
+      provide: TUI_BUTTON_OPTIONS,
+      useValue: {
+        appearance: 'outline',
+        size: 's',
+      },
+    },
+  ],
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  isUser$ = of(false).pipe(shareReplay(1))
+}

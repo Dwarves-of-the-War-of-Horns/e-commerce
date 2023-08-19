30137f5d7cc905480b8e3d28641a0576341e7699
ashtotakoe <egormartynov12@gmail.com>
2023-08-19 19:28:19 +0300

    fix(header): change layout (NO-JIRA)

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -1,45 +1,17 @@
-import { CommonModule } from '@angular/common'
+import { CommonModule, NgOptimizedImage } from '@angular/common'
 import { Component } from '@angular/core'
 import { RouterLink } from '@angular/router'
-import { TUI_BUTTON_OPTIONS, TuiButtonModule, TuiSvgModule } from '@taiga-ui/core'
+import { TuiButtonModule, TuiSvgModule } from '@taiga-ui/core'
 import { TuiTabsModule } from '@taiga-ui/kit'
 import { of, shareReplay } from 'rxjs'
-
-import type { PageData } from '../../models/page-data'
 
 @Component({
   selector: 'ec-header',
   standalone: true,
-  imports: [RouterLink, TuiButtonModule, TuiSvgModule, CommonModule, TuiTabsModule],
+  imports: [RouterLink, TuiButtonModule, TuiSvgModule, CommonModule, TuiTabsModule, NgOptimizedImage],
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
-  providers: [
-    {
-      provide: TUI_BUTTON_OPTIONS,
-      useValue: {
-        appearance: 'outline',
-        size: 's',
-      },
-    },
-  ],
 })
 export class HeaderComponent {
-  isUser$ = of(false).pipe(shareReplay(1))
-
-  pagesData: PageData[] = [
-    {
-      title: 'Home',
-      routerLink: ['/'],
-    },
-
-    {
-      title: 'About us',
-      routerLink: ['/', 'about'],
-    },
-
-    {
-      title: 'Our goodies',
-      routerLink: ['/', 'catalog'],
-    },
-  ]
+  isUser$ = of(true).pipe(shareReplay(1))
 }

7c2d3342d06928ddae3bb72f8cd0ec3399a0ec29
DeePenguin <alivemushroom@gmail.com>
2023-08-21 01:27:36 +0400

    test(header): add provider for auth facade (EC-46)

--- src/app/core/components/header/header.component.spec.ts
+++ src/app/core/components/header/header.component.spec.ts
@@ -1,10 +1,12 @@
 import { type ComponentFixture, TestBed } from '@angular/core/testing'
 import { RouterModule } from '@angular/router'
 import { RouterTestingModule } from '@angular/router/testing'
+import { provideMockStore } from '@ngrx/store/testing'
 import { TuiButtonModule, TuiSvgModule } from '@taiga-ui/core'
 import { TuiTabsModule } from '@taiga-ui/kit'
 
 import { HeaderComponent } from './header.component'
+import { AuthFacade } from 'src/app/auth/auth-store/auth.facade'
 
 describe('HeaderComponent', () => {
   let component: HeaderComponent
@@ -13,6 +15,7 @@ describe('HeaderComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [HeaderComponent, RouterModule, RouterTestingModule, TuiButtonModule, TuiSvgModule, TuiTabsModule],
+      providers: [provideMockStore({}), AuthFacade],
     }).compileComponents()
 
     fixture = TestBed.createComponent(HeaderComponent)

9a0ef9962425dc19ce496b67f6d1e209f2bf17b0
ADyBaH <Brandon4@mail.ru>
2023-08-21 00:34:55 +0300

    feat(auth): add guards (NO-JIRA)
    * feat(auth): add style for sign-in, sign-up(NO-JIRA)
    
    * feat(auth): add guards for sign-in and sign-up(NO-JIRA)
    
    * feat(auth): add link to the login page(EC-39)
    
    * feat(auth): add guards to auth.routes(NO-JIRA)
    
    * feat(auth): add alerts to auth effets(NO-JIRA)
    
    * refactor(auth): change store to authFacade(NO-JIRA)
    
    * refactor(auth): change auth.facade(NO-JIRA)
    
    * feat(auth): add signIn to auth.reducer(NO-JIRA)
    
    * refactor(auth): change sign-in-form html(NO-JIRA)
    
    * feat(auth): add alertAuth dictionary(NO-JIRA)
    
    * fix(auth): return authInitEffect(NO-JIRA)
    
    * fix(auth): return authInitReducer(NO-JIRA)

--- src/app/auth/auth-store/auth.facade.ts
+++ src/app/auth/auth-store/auth.facade.ts
@@ -1,5 +1,4 @@
 import { inject, Injectable } from '@angular/core'
-import { Router } from '@angular/router'
 import type { MyCustomerDraft } from '@commercetools/platform-sdk'
 import type { UserAuthOptions } from '@commercetools/sdk-client-v2'
 import { Store } from '@ngrx/store'
@@ -12,26 +11,15 @@ import { signUpPageActions } from './sign-up-page.actions'
 @Injectable()
 export class AuthFacade {
   private store$ = inject(Store)
-  private router = inject(Router)
   public errorMessage$ = this.store$.select(selectError)
   public isUserLogined$ = this.store$.select(selectIsLogined)
 
-  private subscribeToUserLogin(): void {
-    this.isUserLogined$.subscribe(isUserLogined => {
-      if (isUserLogined) {
-        void this.router.navigate(['home'])
-      }
-    })
-  }
-
   public signIn(customer: UserAuthOptions): void {
     this.store$.dispatch(signInPageActions.signIn({ customer }))
-    this.subscribeToUserLogin()
   }
 
   public signUp(customer: MyCustomerDraft): void {
     this.store$.dispatch(signUpPageActions.signUp({ customer }))
-    this.subscribeToUserLogin()
   }
 
   public init(): void {

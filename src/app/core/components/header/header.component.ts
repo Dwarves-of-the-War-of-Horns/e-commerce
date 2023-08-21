092856ea4a6d3f4558210bddb10e941f17805842
DeePenguin <alivemushroom@gmail.com>
2023-08-21 19:00:32 +0400

    refactor(header): change auth related buttons (NO-JIRA)

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -1,7 +1,7 @@
 import { CommonModule, NgOptimizedImage } from '@angular/common'
-import { Component, inject } from '@angular/core'
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core'
 import { RouterLink } from '@angular/router'
-import { TuiButtonModule, TuiSvgModule } from '@taiga-ui/core'
+import { TuiButtonModule, TuiDataListModule, TuiHostedDropdownModule, TuiSvgModule } from '@taiga-ui/core'
 import { TuiTabsModule } from '@taiga-ui/kit'
 
 import { AuthFacade } from 'src/app/auth/auth-store/auth.facade'
@@ -9,15 +9,30 @@ import { AuthFacade } from 'src/app/auth/auth-store/auth.facade'
 @Component({
   selector: 'ec-header',
   standalone: true,
-  imports: [RouterLink, TuiButtonModule, TuiSvgModule, CommonModule, TuiTabsModule, NgOptimizedImage],
+  imports: [
+    RouterLink,
+    TuiButtonModule,
+    TuiSvgModule,
+    CommonModule,
+    TuiTabsModule,
+    NgOptimizedImage,
+    TuiDataListModule,
+    TuiHostedDropdownModule,
+  ],
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class HeaderComponent {
   private authFacade = inject(AuthFacade)
   public isUserLogined$ = this.authFacade.isUserLogined$
+  public isAuthDropdownOpen = false
 
   public logout(): void {
     this.authFacade.logOut()
+  }
+
+  public selectAuthOption(): void {
+    this.isAuthDropdownOpen = false
   }
 }

356d74644683349b0c4a27b81787a384d87af84e
ADyBaH <Brandon4@mail.ru>
2023-08-25 01:36:31 +0300

    refactor(core): move auth init to app component(NO-JIRA)
    * refactor(core): move auth init to app component(NO-JIRA)
    
    * fix(app): add providers for test(NO-JIRA)

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,26 +1,4 @@
-import { inject, NgModule } from '@angular/core'
-import { EffectsModule } from '@ngrx/effects'
-import { StoreModule } from '@ngrx/store'
+import { NgModule } from '@angular/core'
 
-import { AuthEffects } from '../auth/auth-store/auth.effects'
-import { AuthFacade } from '../auth/auth-store/auth.facade'
-import { authReducer } from '../auth/auth-store/auth.reducer'
-import { StoreFeatureNames } from '../shared/enum/store-feature-names.enum'
-import { StorageModule } from './storage/storage.module'
-import { storageKeyPrefix } from './storage/tokens/storage-key.token'
-
-@NgModule({
-  imports: [
-    EffectsModule.forFeature([AuthEffects]),
-    StoreModule.forFeature(StoreFeatureNames.Auth, authReducer),
-    StorageModule.forRoot({ config: { prefix: storageKeyPrefix.toString() } }),
-  ],
-  providers: [{ provide: AuthFacade, useClass: AuthFacade }],
-})
-export class CoreModule {
-  private authFacade: AuthFacade = inject(AuthFacade)
-
-  constructor() {
-    this.authFacade.init()
-  }
-}
+@NgModule({})
+export class CoreModule {}

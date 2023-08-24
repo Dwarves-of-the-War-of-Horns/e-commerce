ff16b8fb187f385d046da1e505587c17274c9a7a
ADyBaH <Brandon4@mail.ru>
2023-08-24 16:49:14 +0300

    fix(auth): change postal code validator(NO-JIRA)

---
+++ src/app/shared/enum/form-value.enum.ts
@@ -0,0 +1,5 @@
+/* eslint-disable @typescript-eslint/naming-convention */
+export const enum FormFields {
+  Country = 'country',
+  BillingCountry = 'billingCountry',
+}
--- src/app/shared/validators/postal-code.validator.ts
+++ src/app/shared/validators/postal-code.validator.ts
@@ -1,8 +1,12 @@
 import type { AbstractControl, ValidationErrors } from '@angular/forms'
 
 import { countryRegExpDictionary } from '../dictionary/countryRegExpDictionary'
+import { FormFields } from '../enum/form-value.enum'
 
-export const postalCodeValidator = (control: AbstractControl): ValidationErrors | null => {
+export const postalCodeValidator = (
+  control: AbstractControl,
+  fieldName = FormFields.Country,
+): ValidationErrors | null => {
   const isValid = Boolean(control.value)
 
   if (!isValid) {
@@ -10,7 +14,7 @@ export const postalCodeValidator = (control: AbstractControl): ValidationErrors 
   }
 
   // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
-  const country = (control.parent?.value.country as string) ?? 'USA'
+  const country = (control.parent?.value[fieldName] as string) ?? 'USA'
 
   return countryRegExpDictionary[country.toLocaleLowerCase()](control.value as string)
 }

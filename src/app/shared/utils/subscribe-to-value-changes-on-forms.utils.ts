54c338454411818a37e3a611d7da8a07236c809f
ADyBaH <Brandon4@mail.ru>
2023-08-25 22:00:39 +0300

    refactor(app): change path for util function(NO-JIRA)

--- src/app/shared/utils/subscribe-to-value-changes-on-forms.utils.ts
+++ src/app/shared/utils/subscribe-to-value-changes-on-forms.utils.ts
@@ -1,11 +1,10 @@
 import type { FormControl } from '@angular/forms'
 import type { Subscription } from 'rxjs'
 
-export const subscribeToValueChangesOnForms = (arrayForms: FormControl[]): Subscription[] => {
-  return arrayForms.map(
+export const subscribeToValueChangesOnForms = (arrayForms: FormControl[]): Subscription[] =>
+  arrayForms.map(
     (fromControl): Subscription =>
       fromControl.valueChanges.subscribe(() => {
         fromControl.markAsTouched()
       }),
   )
-}

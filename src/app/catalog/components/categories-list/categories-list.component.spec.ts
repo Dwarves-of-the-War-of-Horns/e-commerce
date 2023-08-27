254c9b8ee116c981bd52452da2655d303f38b512
DeePenguin <alivemushroom@gmail.com>
2023-08-28 02:11:44 +0400

    test(catalog): add missing imports (NO-JIRA)

--- src/app/catalog/components/categories-list/categories-list.component.spec.ts
+++ src/app/catalog/components/categories-list/categories-list.component.spec.ts
@@ -1,5 +1,7 @@
 import { type ComponentFixture, TestBed } from '@angular/core/testing'
+import { provideMockStore } from '@ngrx/store/testing'
 
+import { CatalogFacade } from '../../catalog-store/services/catalog.facade'
 import { CategoriesListComponent } from './categories-list.component'
 
 describe('CategoriesListComponent', () => {
@@ -9,6 +11,7 @@ describe('CategoriesListComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [CategoriesListComponent],
+      providers: [provideMockStore({}), CatalogFacade],
     }).compileComponents()
 
     fixture = TestBed.createComponent(CategoriesListComponent)

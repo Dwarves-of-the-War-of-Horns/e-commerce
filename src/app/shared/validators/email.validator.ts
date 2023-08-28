8899b366a989fdc7afc151dab4ea9dc5c8e644ae
ADyBaH <Brandon4@mail.ru>
2023-08-28 20:57:40 +0300

    refactor(shared): change name for validatos(NO-JIRA)

--- src/app/shared/validators/email.validator.ts
+++ src/app/shared/validators/email.validator.ts
@@ -1,6 +1,6 @@
 import type { AbstractControl, ValidationErrors } from '@angular/forms'
 
-import { emailRegExp } from '../constants/regExp'
+import { emailRegExp } from '../constants/regExp.constant'
 
 export const emailValidator = (control: AbstractControl): ValidationErrors | null => {
   const isValid = emailRegExp.test(control.value as string)

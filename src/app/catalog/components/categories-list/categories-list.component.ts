14b59b380e0427ea5d2502f388dc3f36ccb51adb
DeePenguin <alivemushroom@gmail.com>
2023-08-29 18:23:37 +0400

    feat(categories): add custom tree view (NO-JIRA)

--- src/app/catalog/components/categories-list/categories-list.component.ts
+++ src/app/catalog/components/categories-list/categories-list.component.ts
@@ -1,15 +1,31 @@
-import { Component, type OnInit } from '@angular/core'
+import { ChangeDetectionStrategy, Component, type OnInit } from '@angular/core'
+import type { TuiHandler } from '@taiga-ui/cdk'
+import { TUI_TREE_CONTENT } from '@taiga-ui/kit'
+import { PolymorpheusComponent } from '@tinkoff/ng-polymorpheus'
 
 import { CatalogFacade } from '../../catalog-store/services/catalog.facade'
+import { TreeComponent } from '../tree/tree.component'
+import type { SimpleCategory } from 'src/app/shared/models/simple-category.model'
 
 @Component({
   selector: 'ec-categories-list',
   templateUrl: './categories-list.component.html',
   styleUrls: ['./categories-list.component.scss'],
+  providers: [
+    {
+      provide: TUI_TREE_CONTENT,
+      useValue: new PolymorpheusComponent(TreeComponent),
+    },
+  ],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class CategoriesListComponent implements OnInit {
+  public categories$ = this.catalogFacade.categories$
+
   constructor(private catalogFacade: CatalogFacade) {}
   public ngOnInit(): void {
     this.catalogFacade.initCatalog()
   }
+
+  public handler: TuiHandler<SimpleCategory, readonly SimpleCategory[]> = category => category.children
 }

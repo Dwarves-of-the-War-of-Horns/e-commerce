1fedcd06655c9423a041b9b44aaf73cf078d3cf7
DeePenguin <alivemushroom@gmail.com>
2023-08-30 20:28:49 +0400

    refactor(catalog): move init catalog to catalog page (NO-JIRA)

--- src/app/catalog/pages/catalog-page/catalog-page.component.ts
+++ src/app/catalog/pages/catalog-page/catalog-page.component.ts
@@ -1,8 +1,15 @@
-import { Component } from '@angular/core'
+import { Component, type OnInit } from '@angular/core'
+
+import { CatalogFacade } from '../../catalog-store/services/catalog.facade'
 
 @Component({
   selector: 'ec-catalog-page',
   templateUrl: './catalog-page.component.html',
   styleUrls: ['./catalog-page.component.scss'],
 })
-export class CatalogPageComponent {}
+export class CatalogPageComponent implements OnInit {
+  constructor(private catalogFacade: CatalogFacade) {}
+  public ngOnInit(): void {
+    this.catalogFacade.initCatalog()
+  }
+}

c632b71bbcd84345c3fa39c34e22ab338150433c
ADyBaH <Brandon4@mail.ru>
2023-08-30 13:02:09 +0300

    feat(user): add edit mode for user(EC-62)

--- src/app/user/user.component.spec.ts
+++ src/app/user/user.component.spec.ts
@@ -1,5 +1,10 @@
+import { CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA } from '@angular/core'
 import { type ComponentFixture, TestBed } from '@angular/core/testing'
+import { provideMockStore } from '@ngrx/store/testing'
 
+import { AuthFacade } from '../auth/auth-store/auth.facade'
+import { UserAddressesComponent } from './components/user-addresses/user-addresses.component'
+import { UserInformationComponent } from './components/user-information/user-information.component'
 import { UserComponent } from './user.component'
 
 describe('UserComponent', () => {
@@ -8,7 +13,9 @@ describe('UserComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [UserComponent],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA],
+      declarations: [UserComponent, UserAddressesComponent, UserInformationComponent],
+      providers: [provideMockStore({}), AuthFacade],
     }).compileComponents()
 
     fixture = TestBed.createComponent(UserComponent)

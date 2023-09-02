398dbe9d4cc75e8cc9113af63b0ab8ffb3c40a77
DeePenguin <alivemushroom@gmail.com>
2023-09-02 12:21:49 +0400

    feat(product-card): add more styles & hover effects (EC-52)

--- src/app/catalog/components/product-card/product-card.component.ts
+++ src/app/catalog/components/product-card/product-card.component.ts
@@ -11,7 +11,7 @@ import { SimpleProduct } from 'src/app/shared/models/simple-product.model'
 export class ProductCardComponent {
   @Input() public product!: SimpleProduct
 
-  public maxDescriptionLength = 200
+  public maxDescriptionLength = 100
 
   public cutDescription(description: string): string {
     return description.slice(0, this.maxDescriptionLength)

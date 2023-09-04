8cac8210333091e14925b3850c32e30c69fa6a1a
DeePenguin <alivemushroom@gmail.com>
2023-09-04 04:10:46 +0400

    refactor(catalog): move atrribute value to separate interface (NO-JIRA)

--- src/app/core/commercetools/helpers/convert-attribute-to-simple-attribute.helper.ts
+++ src/app/core/commercetools/helpers/convert-attribute-to-simple-attribute.helper.ts
@@ -1,9 +1,10 @@
 import type { AttributeDefinition, AttributeType } from '@commercetools/platform-sdk'
 
 import { locale } from 'src/app/shared/constants/locale'
+import type { AttributeValue } from 'src/app/shared/models/attribute-value.model'
 import type { SimpleAttribute } from 'src/app/shared/models/simple-attribute.model'
 
-const getValues = (type: AttributeType): Array<{ key: string; label: string }> => {
+const getValues = (type: AttributeType): AttributeValue[] => {
   if (type.name === 'enum') {
     return type.values
   }
--- src/app/shared/models/simple-attribute.model.ts
+++ src/app/shared/models/simple-attribute.model.ts
@@ -1,8 +1,7 @@
+import type { AttributeValue } from './attribute-value.model'
+
 export interface SimpleAttribute {
   name: string
   label: string
-  values: Array<{
-    key: string
-    label: string
-  }>
+  values: AttributeValue[]
 }

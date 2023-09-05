ca9223e47949a885595d44e4273c5e26a2411fba
Kristina <37124027+DeePenguin@users.noreply.github.com>
2023-09-05 23:06:04 +0400

    fix(auth): change token after password change (NO-JIRA) (#82)

--- src/app/auth/auth-store/actions/change-password-page.action.ts
+++ src/app/auth/auth-store/actions/change-password-page.action.ts
@@ -1,9 +1,10 @@
-import type { MyCustomerChangePassword } from '@commercetools/platform-sdk'
 import { createActionGroup, props } from '@ngrx/store'
+
+import type { ChangePasswordProps } from 'src/app/shared/models/change-password-props.model'
 
 export const changePasswordPageActions = createActionGroup({
   source: 'Change Password Page',
   events: {
-    'Change Password': props<{ newPassword: MyCustomerChangePassword }>(),
+    'Change Password': props<{ newPassword: ChangePasswordProps }>(),
   },
 })

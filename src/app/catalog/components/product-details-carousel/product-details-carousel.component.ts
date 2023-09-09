46333e85640704bef830b9c769753f5544e2d35e
DeePenguin <alivemushroom@gmail.com>
2023-09-09 20:51:15 +0400

    refactor(product-details): unsubscribe from dialog (NO-JIRA)

--- src/app/catalog/components/product-details-carousel/product-details-carousel.component.ts
+++ src/app/catalog/components/product-details-carousel/product-details-carousel.component.ts
@@ -1,16 +1,18 @@
-import { Component, Inject, Input } from '@angular/core'
+import { Component, Inject, Input, type OnDestroy } from '@angular/core'
 import { type TuiDialogContext, TuiDialogService } from '@taiga-ui/core'
 import type { PolymorpheusContent } from '@tinkoff/ng-polymorpheus'
+import type { Subscription } from 'rxjs'
 
 @Component({
   selector: 'ec-product-details-carousel',
   templateUrl: './product-details-carousel.component.html',
   styleUrls: ['./product-details-carousel.component.scss'],
 })
-export class ProductDetailsCarouselComponent {
+export class ProductDetailsCarouselComponent implements OnDestroy {
   @Input() public images: string[] = []
 
   public carouselActiveIndex = 0
+  private dialog!: Subscription
 
   constructor(@Inject(TuiDialogService) private readonly dialogs: TuiDialogService) {}
 
@@ -19,10 +21,14 @@ export class ProductDetailsCarouselComponent {
   }
 
   public openDialog(content: PolymorpheusContent<TuiDialogContext>): void {
-    this.dialogs
+    this.dialog = this.dialogs
       .open(content, {
         size: 'fullscreen',
       })
       .subscribe()
+  }
+
+  public ngOnDestroy(): void {
+    this.dialog.unsubscribe()
   }
 }

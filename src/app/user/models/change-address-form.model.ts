7ead6a7b61346b2643e801cae759cfbba45a5407
ADyBaH <Brandon4@mail.ru>
2023-09-09 15:43:07 +0300

    refactor(user): change name for transformAddressSubmitForm(NO-JIRA)

--- src/app/user/models/change-address-form.model.ts
+++ src/app/user/models/change-address-form.model.ts
@@ -2,7 +2,7 @@ import type { FormGroup } from '@angular/forms'
 
 import type { AddressActionSubmitForm } from '../types/address-action-submit-form.type'
 
-export interface TransformAddressSubmitForm {
+export interface ChangeAddressForm {
   form: FormGroup
   version: number
   action: AddressActionSubmitForm

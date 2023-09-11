594dc92b47f63f64ca9dba1a99bc4d86dd491de2
DeePenguin <alivemushroom@gmail.com>
2023-09-11 04:21:08 +0400

    feat(catalog): add add to cart button in product cards (EC-113)

--- src/app/catalog/components/product-card/product-card.component.ts
+++ src/app/catalog/components/product-card/product-card.component.ts
@@ -10,10 +10,17 @@ import { SimpleProduct } from 'src/app/shared/models/simple-product.model'
 })
 export class ProductCardComponent {
   @Input() public product!: SimpleProduct
+  @Input() public addToCartHandler!: ({ productId, variantId }: { productId: string; variantId: number }) => void
+  @Input() public isInCart!: boolean | null
 
   public maxDescriptionLength = 100
 
   public cutDescription(description: string): string {
     return description.slice(0, this.maxDescriptionLength)
+  }
+
+  public addToCart(event: MouseEvent): void {
+    event.stopPropagation()
+    this.addToCartHandler({ productId: this.product.id, variantId: this.product.variantId })
   }
 }

043bfef3a9863b7f4575a9f1ab7e680df337a944
DeePenguin <alivemushroom@gmail.com>
2023-09-11 05:38:59 +0400

    feat(catalog): add add to cart button in product page (EC-116)

--- src/app/catalog/components/product-details/product-details.component.ts
+++ src/app/catalog/components/product-details/product-details.component.ts
@@ -1,14 +1,46 @@
-import { Component } from '@angular/core'
+import { ChangeDetectionStrategy, Component, Inject, Self } from '@angular/core'
+import { TuiDestroyService } from '@taiga-ui/cdk'
+import { combineLatest, filter, map, takeUntil } from 'rxjs'
 
 import { CatalogFacade } from '../../catalog-store/services/catalog.facade'
+import { CartFacade } from 'src/app/cart/cart-store/services/cart.facade'
 
 @Component({
   selector: 'ec-product-details',
   templateUrl: './product-details.component.html',
   styleUrls: ['./product-details.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  providers: [TuiDestroyService],
 })
 export class ProductDetailsComponent {
   public productDetails$ = this.catalogFacade.productDetails$
+  public productsInCart$ = this.cartFacade.productsInCart$
+  public isCartLoading$ = this.cartFacade.isLoading$
+  public productIds!: { productId: string; variantId: number }
+  public isInCart!: boolean
 
-  constructor(private catalogFacade: CatalogFacade) {}
+  constructor(
+    private catalogFacade: CatalogFacade,
+    private cartFacade: CartFacade,
+    @Self()
+    @Inject(TuiDestroyService)
+    private destroy$: TuiDestroyService,
+  ) {
+    combineLatest([this.productDetails$, this.productsInCart$])
+      .pipe(map(([productDetails, productsInCart]) => ({ productDetails, productsInCart })))
+      .pipe(
+        takeUntil(this.destroy$),
+        filter(({ productDetails, productsInCart }) => Boolean(productDetails) && Boolean(productsInCart)),
+      )
+      .subscribe(({ productDetails, productsInCart }) => {
+        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+        this.productIds = { productId: productDetails!.id, variantId: productDetails!.variantId }
+        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+        this.isInCart = productsInCart!.some(product => product.productId === this.productIds.productId)
+      })
+  }
+
+  public addToCart(): void {
+    this.cartFacade.addProductToCart(this.productIds)
+  }
 }

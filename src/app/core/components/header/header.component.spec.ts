d8ca75657f75f2f21fbd3755387861b24a144b9e
Kristina <37124027+DeePenguin@users.noreply.github.com>
2023-09-12 20:46:39 +0400

    feat(header): add cart badge (EC-131)

--- src/app/core/components/header/header.component.spec.ts
+++ src/app/core/components/header/header.component.spec.ts
@@ -2,10 +2,13 @@ import { type ComponentFixture, TestBed } from '@angular/core/testing'
 import { RouterModule } from '@angular/router'
 import { RouterTestingModule } from '@angular/router/testing'
 import { provideMockStore } from '@ngrx/store/testing'
+import { TuiLetModule } from '@taiga-ui/cdk'
 import { TuiButtonModule, TuiSvgModule } from '@taiga-ui/core'
+import { TuiBadgedContentModule } from '@taiga-ui/kit'
 
 import { HeaderComponent } from './header.component'
 import { AuthFacade } from 'src/app/auth/auth-store/service/auth.facade'
+import { CartFacade } from 'src/app/cart/cart-store/services/cart.facade'
 
 describe('HeaderComponent', () => {
   let component: HeaderComponent
@@ -13,8 +16,16 @@ describe('HeaderComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [HeaderComponent, RouterModule, RouterTestingModule, TuiButtonModule, TuiSvgModule],
-      providers: [provideMockStore({}), AuthFacade],
+      imports: [
+        HeaderComponent,
+        RouterModule,
+        RouterTestingModule,
+        TuiButtonModule,
+        TuiSvgModule,
+        TuiBadgedContentModule,
+        TuiLetModule,
+      ],
+      providers: [provideMockStore({}), AuthFacade, CartFacade],
     }).compileComponents()
 
     fixture = TestBed.createComponent(HeaderComponent)

078215cf56350582e3f0c02de2b2e016162181bb
ashtotakoe <egormartynov12@gmail.com>
2023-09-16 06:13:57 +0400

    refactor(about): add imports to test file (NO-JIRA)

--- src/app/about/about-page.component.spec.ts
+++ src/app/about/about-page.component.spec.ts
@@ -1,6 +1,8 @@
 import { type ComponentFixture, TestBed } from '@angular/core/testing'
+import { TuiIslandModule } from '@taiga-ui/kit'
 
 import { AboutPageComponent } from './about-page.component'
+import { ExtractGithubNickPipe } from './pipes/extract-github-nick.pipe'
 
 describe('AboutPageComponent', () => {
   let component: AboutPageComponent
@@ -8,7 +10,8 @@ describe('AboutPageComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [AboutPageComponent],
+      declarations: [AboutPageComponent, ExtractGithubNickPipe],
+      imports: [TuiIslandModule],
     }).compileComponents()
 
     fixture = TestBed.createComponent(AboutPageComponent)

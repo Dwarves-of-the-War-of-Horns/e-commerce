cda380db9a30b61d8eb16697274689d8bd0c57d5
ashtotakoe <egormartynov12@gmail.com>
2023-09-16 06:08:54 +0400

    feat(about): add collaborators introduction (EC-127)

--- src/app/about/about.module.ts
+++ src/app/about/about.module.ts
@@ -1,11 +1,13 @@
 import { CommonModule } from '@angular/common'
 import { NgModule } from '@angular/core'
+import { TuiIslandModule } from '@taiga-ui/kit'
 
 import { AboutPageComponent } from './about-page.component'
 import { AboutRoutingModule } from './about-routing.module'
+import { ExtractGithubNickPipe } from './pipes/extract-github-nick.pipe'
 
 @NgModule({
-  declarations: [AboutPageComponent],
-  imports: [CommonModule, AboutRoutingModule],
+  declarations: [AboutPageComponent, ExtractGithubNickPipe],
+  imports: [CommonModule, AboutRoutingModule, TuiIslandModule],
 })
 export class AboutModule {}

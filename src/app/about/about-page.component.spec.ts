0167c7acc488bda77970db107901faa28c216b95
ashtotakoe <egormartynov12@gmail.com>
2023-09-16 16:06:15 +0400

    feat(about): add course logo (EC-136)

--- src/app/about/about-page.component.spec.ts
+++ src/app/about/about-page.component.spec.ts
@@ -1,6 +1,10 @@
 import { type ComponentFixture, TestBed } from '@angular/core/testing'
+import { TuiIslandModule } from '@taiga-ui/kit'
 
 import { AboutPageComponent } from './about-page.component'
+import { CollaboratorsListComponent } from './components/collaborators-list/collaborators-list.component'
+import { CourseLogoComponent } from './components/course-logo/course-logo.component'
+import { ExtractGithubNickPipe } from './pipes/extract-github-nick.pipe'
 
 describe('AboutPageComponent', () => {
   let component: AboutPageComponent
@@ -8,7 +12,8 @@ describe('AboutPageComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [AboutPageComponent],
+      declarations: [AboutPageComponent, CollaboratorsListComponent, ExtractGithubNickPipe, CourseLogoComponent],
+      imports: [TuiIslandModule],
     }).compileComponents()
 
     fixture = TestBed.createComponent(AboutPageComponent)
--- src/app/about/about.module.ts
+++ src/app/about/about.module.ts
@@ -1,14 +1,15 @@
 import { CommonModule } from '@angular/common'
 import { NgModule } from '@angular/core'
-import { TuiIslandModule } from '@taiga-ui/kit'
+import { TuiActionModule, TuiIslandModule } from '@taiga-ui/kit'
 
 import { AboutPageComponent } from './about-page.component'
 import { AboutRoutingModule } from './about-routing.module'
 import { CollaboratorsListComponent } from './components/collaborators-list/collaborators-list.component'
+import { CourseLogoComponent } from './components/course-logo/course-logo.component'
 import { ExtractGithubNickPipe } from './pipes/extract-github-nick.pipe'
 
 @NgModule({
-  declarations: [AboutPageComponent, ExtractGithubNickPipe, CollaboratorsListComponent],
-  imports: [CommonModule, AboutRoutingModule, TuiIslandModule],
+  declarations: [AboutPageComponent, ExtractGithubNickPipe, CollaboratorsListComponent, CourseLogoComponent],
+  imports: [CommonModule, AboutRoutingModule, TuiIslandModule, TuiActionModule],
 })
 export class AboutModule {}
